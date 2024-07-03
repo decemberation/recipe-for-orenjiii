3bdea3940e7f7141419eb721bd4a171de06d9d16
decemberation <48883241+decemberation@users.noreply.github.com>
2024-07-02 23:14:06 -0700

    Update index.js
    Change cors origin

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const {notFound, errorHandler} = require('./middleware/errorMiddleware');
 const app = express();
 app.use(express.json({extended: true}));
 app.use(express.urlencoded({extended: true}));
-app.use(cors({credentials: true, origin: "*"}));
+app.use(cors({credentials: true, origin: "https://recipe-for-orenjiii.onrender.com"}));
 app.use(upload());
 
 app.use('/uploads', express.static(__dirname + '/uploads'));
